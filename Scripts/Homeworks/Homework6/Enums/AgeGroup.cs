2ade8e906ec60d436d1e9e6c4536b914769bdd74
inviqDev <inviq.dev@gmail.com>
2025-05-27 14:57:59 +0300

    Final adjustments

--- Scripts/Homeworks/Homework6/Enums/AgeGroup.cs
+++
@@ -1,8 +0,0 @@
-namespace IT_Academy_CSharp.Homework6;
-
-public enum AgeGroup
-{
-    Apopo,
-    Popopo,
-    Asdasf,
-}
--- Scripts/Program.cs
+++ Scripts/Program.cs
@@ -1,12 +1,9 @@
-using IT_Academy_CSharp.Homework6;
-
 namespace IT_Academy_CSharp;
 
 internal static class Program
 {
     private static void Main()
     {
-        Homework6Main.RunHomework6();
         
         Console.ReadKey();
     }
